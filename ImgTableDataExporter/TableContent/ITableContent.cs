abb1dd027105332c6152c4ca87cba3c23b9bb4c9
Liam M <lmcrailt4@gmail.com>
2022-07-05 21:13:31 +0100

    Added new basic content types and interface.

---
+++ ImgTableDataExporter/TableContent/ITableContent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImgTableDataExporter.TableContent
+{
+	/// <summary>
+	/// This interface represents one type of content located within a cell.
+	/// </summary>
+	public interface ITableContent
+	{
+		void WriteContent();
+	}
+}
---
+++ ImgTableDataExporter/TableContent/TextContent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImgTableDataExporter.TableContent
+{
+	/// <summary>
+	/// Represents the content of one cell. This content type holds a string used to write plain text inside of a cell.
+	/// </summary>
+	public class TextContent : ITableContent
+	{
+		public string Content { get; set; }
+
+		public void WriteContent()
+		{
+			
+		}
+	}
+}

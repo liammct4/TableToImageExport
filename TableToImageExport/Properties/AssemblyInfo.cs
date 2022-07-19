1f706f861ab18ae9abaa524dcf662a07ecbb9960
Liam M <lmcrailt4@gmail.com>
2022-07-19 20:54:59 +0100

    Added XML documentation to nuget package.

--- TableToImageExport/Properties/AssemblyInfo.cs
+++ TableToImageExport/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TableToImageExport")]
-[assembly: AssemblyDescription("A C# library for processing csv/tabular data into images.")]
+[assembly: AssemblyDescription("A C# library for processing tabular CSV/TSV data into images.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Liam McRailt")]
 [assembly: AssemblyProduct("TableToImageExport")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]

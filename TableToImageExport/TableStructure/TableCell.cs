907560e1306d89d9f20235ca5a7947f2d2295687
Liam M <lmcrailt4@gmail.com>
2022-10-09 16:51:06 +0100

    Added base column span and row span properties with validation and comments.

--- TableToImageExport/TableStructure/TableCell.cs
+++ TableToImageExport/TableStructure/TableCell.cs
@@ -17,6 +17,8 @@ namespace TableToImageExport.TableStructure
 	public class TableCell : Cell
 	{
 		public static Size DefaultCellSize = new(100, 28);
+		public static int DefaultColumnSpan = 1;
+		public static int DefaultRowSpan = 1;
 
 		/// <summary>
 		/// The size of this cell in pixels, generally, you shouldn't need to use this as you can use the universal properties <see cref="TableRow.Height"/> and <see cref="TableColumn.Width"/> to set the size of every cell.<br/><br/>
@@ -24,6 +26,42 @@ namespace TableToImageExport.TableStructure
 		/// If this cell is wider then other cells in the same column, the other cells will still be aligned properly however, they will have a large bit of empty space between the columns. This also applies to rows.
 		/// </summary>
 		public Size CellSize { get; set; } = DefaultCellSize;
+
+		/// <summary>
+		/// How many columns this cell is allowed to overlap over (right). Cannot be less than 1.
+		/// </summary>
+		public int ColumnSpan
+		{
+			get => _columnSpan;
+			set
+			{
+				if (value < 1)
+				{
+					throw new ArgumentOutOfRangeException(nameof(ColumnSpan), "The columnspan cannot be negative or less than 1.");
+				}
+
+				_columnSpan = value;
+			}
+		}
+		private int _columnSpan = DefaultColumnSpan;
+
+		/// <summary>
+		/// How many rows this cell is allowed to overlap over (below). Cannot be less than 1.
+		/// </summary>
+		public int RowSpan
+		{
+			get => _rowSpan;
+			set
+			{
+				if (value < 1)
+				{
+					throw new ArgumentOutOfRangeException(nameof(RowSpan), "The rowspan cannot be negative or less than 1.");
+				}
+
+				_rowSpan = value;
+			}
+		}
+		private int _rowSpan = DefaultRowSpan;
 		/// <summary>
 		/// Do NOT use this method to create a cell, instead use the relevant CreateNewCell method from a <see cref="TableGenerator"/> object. 
 		/// </summary>
